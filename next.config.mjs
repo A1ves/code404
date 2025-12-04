7b1ccc8dc02dbc7540eb1eb0f0e89ff054abad88
A1ves <ayronwork@gmail.com>
2025-12-03 23:51:00 -0300

    alter next.config to add basepath

--- next.config.mjs
+++ next.config.mjs
@@ -10,7 +10,7 @@ const nextConfig = {
   },
   // Se for publicar em subdiretório (ex: username.github.io/code404)
   // descomente a linha abaixo e ajuste o nome do repositório:
-  // basePath: '/code404',
+  basePath: '/code404',
 }
 
 export default nextConfig
